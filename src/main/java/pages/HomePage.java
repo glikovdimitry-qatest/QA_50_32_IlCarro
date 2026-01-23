a954a843033073ac465d214cf379656e578d3029
MedSveta <sveta1978medved@gmail.com>
2026-01-23 12:04:40 +0200

    Registration Positive Test

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -16,8 +16,14 @@ public class HomePage extends BasePage {
 
     @FindBy(xpath = "//a[text()=' Log in ']")
     WebElement btnLogin;
+    @FindBy(xpath = "//a[text()=' Sign up ']")
+    WebElement btnSignUp;
 
     public void clickBtnLogin(){
         btnLogin.click();
+    }
+
+    public void clickBtnSignUp(){
+        btnSignUp.click();
     }
 }
---
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -0,0 +1,34 @@
+package ui_tests;
+
+import dto.User;
+import manager.ApplicationManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.RegistrationPage;
+
+import java.util.Random;
+
+
+public class RegistrationTests extends ApplicationManager {
+    RegistrationPage registrationPage;
+
+    @BeforeMethod
+    public void goToRegistrationPage() {
+        new HomePage(getDriver()).clickBtnSignUp();
+        registrationPage = new RegistrationPage(getDriver());
+    }
+
+    @Test
+    public void registrationPositiveTest() {
+        int i = new Random().nextInt(1000);
+        User user = User.builder()
+                .firstName("UUU")
+                .lastName("PPP")
+                .email("lmkjiu"+i+"@defrt.bhy")
+                .password("Pqwerty453!")
+                .build();
+        registrationPage.typeRegistrationForm(user);
+        registrationPage.clickCheckBox();
+    }
+}
